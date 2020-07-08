4defe2f336d77d5dfeea9ce1e8d8b5e0035adfb8
SUMIT <64258056+malisumit86@users.noreply.github.com>
2020-07-09 00:39:48 +0530

    Updates

---
+++ precache-manifest.5360982296e558e674f589f2f100eb09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced72a50a584dae5b784669f5519baad",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "17f21dc1e775427ccabc",
+    "url": "/todolist/static/css/main.84874051.chunk.css"
+  },
+  {
+    "revision": "fc6bcf256ad07e4cfc5f",
+    "url": "/todolist/static/js/2.642119e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.642119e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f21dc1e775427ccabc",
+    "url": "/todolist/static/js/main.d9b11267.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
